6dba04badb8a3ecc965ff110f64b898a62aae9ca
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-13 14:21:47 -0300

    Init repo

---
+++ AppMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMusic
+//
+//  Created by Jadiê on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
