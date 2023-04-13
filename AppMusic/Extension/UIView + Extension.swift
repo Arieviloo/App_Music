e8af177adbe38c45cf1e1c08dccaed3b58ef3fec
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-13 14:39:46 -0300

    Adding extension and models

---
+++ AppMusic/Extension/UIView + Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView + Extension.swift
+//  AppMusic
+//
+//  Created by Jadiê on 13/04/23.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+    }
+    
+}
