2ea9b71998f703ce1bd36dc10fe3e25051554ccc
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-15 12:04:22 -0300

    Adding method constructor CustomCardView

--- AppMusic/CustomVIew/CustomCardView.swift
+++ AppMusic/CustomVIew/CustomCardView.swift
@@ -14,15 +14,14 @@ enum ViewModel {
 
 class CustomCardView: UIView {
     
-    var vmode: ViewModel?
+    var vmodel: ViewModel?
+    var dataModel: CardViewModel?
     var containerLeadingConstraints: NSLayoutConstraint?
     var containerTrailingConstraints: NSLayoutConstraint?
     var containerTopConstraints: NSLayoutConstraint?
     var containerBottomConstraints: NSLayoutConstraint?
-    var dataModel: CardViewModel?
-
     
-    lazy var cadContainerView: UIView = {
+    lazy var cardContainerView: UIView = {
         let v = UIView()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.clipsToBounds = true
@@ -77,5 +76,88 @@ class CustomCardView: UIView {
         return btn
     }()
     
+    lazy var cardCategoryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var cardCategoryDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 11, weight: .regular)
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var cardTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 31, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var likeAndTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var descriptionTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    init(viewModel: ViewModel, cardData:CardViewModel) {
+        let frame = CGRect.zero
+        self.vmodel = viewModel
+        self.dataModel = cardData
+        super.init(frame: frame)
+        self.addSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubViews() {
+        self.addSubview(self.cardContainerView)
+        self.cardContainerView.addSubview(self.cardImage)
+        self.cardContainerView.addSubview(self.overlayView)
+        self.cardContainerView.addSubview(self.profileBorderView)
+        self.cardContainerView.addSubview(self.cardProfilePicture)
+        self.cardContainerView.addSubview(self.addProfileImageButton)
+        self.cardContainerView.addSubview(self.cardCategoryTitleLabel)
+        self.cardContainerView.addSubview(self.cardCategoryDateLabel)
+        self.cardContainerView.addSubview(self.cardTitleLabel)
+        self.cardContainerView.addSubview(self.descriptionTitleLabel)
+        
+        self.updateLayout(for: self.vmodel ?? .card)
+    }
+    
+    private func updateLayout(for mode: ViewModel) {
+        if mode == .full {
+            self.containerLeadingConstraints?.constant = 0
+            self.containerTrailingConstraints?.constant = 0
+            self.containerTopConstraints?.constant = 0
+            self.containerBottomConstraints?.constant = 0
+            self.descriptionTitleLabel.isHidden = false
+        }else {
+            self.containerLeadingConstraints?.constant = 30
+            self.containerTrailingConstraints?.constant = -30
+            self.containerTopConstraints?.constant = 15
+            self.containerBottomConstraints?.constant = -15
+            self.descriptionTitleLabel.isHidden = true
+        }
+    }
     
 }
