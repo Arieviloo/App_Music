dc61ff45314873c9a5ad90827367ac004063b236
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-04-15 11:23:00 -0300

    Adding CustomCardView

---
+++ AppMusic/CustomVIew/CustomCardView.swift
@@ -0,0 +1,81 @@
+//
+//  CustomCardView.swift
+//  AppMusic
+//
+//  Created by Jadiê on 15/04/23.
+//
+
+import UIKit
+
+enum ViewModel {
+    case full
+    case card
+}
+
+class CustomCardView: UIView {
+    
+    var vmode: ViewModel?
+    var containerLeadingConstraints: NSLayoutConstraint?
+    var containerTrailingConstraints: NSLayoutConstraint?
+    var containerTopConstraints: NSLayoutConstraint?
+    var containerBottomConstraints: NSLayoutConstraint?
+    var dataModel: CardViewModel?
+
+    
+    lazy var cadContainerView: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.clipsToBounds = true
+        v.layer.cornerRadius = 30
+        v.layer.shadowOpacity = 1
+        v.layer.shadowOffset = CGSize(width: 0, height: -2)
+        v.layer.shadowRadius = 20
+        return v
+    }()
+    
+    lazy var cardImage: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFill
+        img.backgroundColor = .black
+        return img
+    }()
+    
+    lazy var overlayView: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        return v
+    }()
+    
+    lazy var profileBorderView: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .clear
+        v.layer.borderWidth = 1
+        v.layer.borderColor = UIColor.white.cgColor
+        v.layer.cornerRadius = 25
+        return v
+    }()
+    
+    lazy var cardProfilePicture: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFill
+        img.backgroundColor = .black
+        img.clipsToBounds = true
+        img.layer.cornerRadius = 20
+        return img
+    }()
+    
+    lazy var addProfileImageButton: UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .white
+        btn.setBackgroundImage(UIImage(named: "plus"), for: .normal)
+        btn.layer.cornerRadius = 10
+        return btn
+    }()
+    
+    
+}
